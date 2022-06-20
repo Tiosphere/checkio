a7ca91a4e4335fdce167f6bd2214f41425d8dc09
Tiosphere <loong_22@hotmail.com>
2022-06-20 14:42:24 +0700

    init

---
+++ firstword.py
@@ -0,0 +1,21 @@
+def first_word(text: str) -> str:
+    """
+    returns the first word in a given text.
+    """
+    index = text.find(" ")
+    if index == -1:
+        return text
+    else:
+        return text[:index]
+    # your code here
+
+
+if __name__ == "__main__":
+    print("Example:")
+    print(first_word("Hello world"))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert first_word("Hello world") == "Hello"
+    assert first_word("a word") == "a"
+    assert first_word("hi") == "hi"
+    print("Coding complete? Click 'Check' to earn cool rewards!")
